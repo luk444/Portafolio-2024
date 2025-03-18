eb7bd60a9005fa366fdc8f500b2f36d5d31838f8
luk444 <lucadesbloqueo20@gmail.com>
2025-03-18 06:10:46 -0300

    add proyects

--- src/data/proyects.js
+++ src/data/proyects.js
@@ -1,35 +1,82 @@
-import baxinova from "../assets/WebCalderas.png";
 import Proyect from "../assets/3dProyect.png";
-import TradeCripto from "../assets/TradeCripto.png";
-import translator from "../assets/translator.png";
-import furniture from "../assets/Forniture-Ecomerce.png";
-import Classyclub from "../assets/ClassyClub.png";
-import Autopart from "../assets/Ecomerce-Automanicaos.png";
+import classyclub from "../assets/ClassyClub.png";
 import idelete from "../assets/idelete.png";
+import calderasweb from "../assets/calderasweb.png";
+import tradeCripto from "../assets/TradeCripto.png";
+import translator from "../assets/translator.png";
+import mueblesweb from "../assets/mueblesweb.png";
+import autopartsweb from "../assets/autopartsweb.png";
+import cursoweb from "../assets/cursoweb.png";
+import webservicedev from "../assets/webservicedev.png";
+import detailscreen from "../assets/detailscreen.png";
 
 
 const proyects = [
   {
-    title: "Development of Auto Parts Ecommerce Platform",
-    date: "Jun 2024",
+    title: "Development SwapORkick (En desarrollo)",
+    date: "Feb 2025",
     description:
-      "I developed a complete ecommerce platform, managing both the front-end and the back-end. The app uses Firebase Auth for authentication, offering separate panels for administrators and end users. Implemented detailed metrics such as order tracking and inventory management, making it easy for administrators to add, edit, and delete products efficiently. The responsive design ensures a consistent user experience across devices, while the integration of payment methods and shipping options ensures secure and efficient transactions.",
-    tecnologies: ["Javascript", "React JS", "Redux", "Tailwind", "Node JS", "Express", "Firebase", "FirebaseAuth"],
-    deploy: "https://automaniacos.netlify.app/",
-    repo: "https://github.com/luk444/Ecomerce-AutoManiacos",
-    img: Autopart,
+      "Develop a p2p exchange platform for CS2 items, in which users can register, link their Steam and Metamask accounts to buy and sell items in our market, charging a minimal commission and also providing excellent service by avoiding scams and making the community a safer place to buy and sell items.",
+    tecnologies: ["Javascript", "React JS", "Tailwind", "Node JS", "Mongodb"],
+    deploy: "https://developer-web-service.netlify.app",
+    repo: "https://github.com/luk444/webservicedev",
+    img: detailscreen,
     rol:"Full Stack Developer"
   },
   {
-    title: "Website development for heating company",
-    date: "Dec 2022",
+    title: "Development service web",
+    date: "Feb 2025",
     description:
-      "I have worked on the Frontend development of web pages for heating contractor companies. My main role has been to create interfaces that allow the company to publish its services on Google and facilitate the coordination of visits with clients through phone calls after they view the relevant information on the website. This job has allowed me to apply my web design and user experience skills to ensure navigation is intuitive and key information about heating services is clearly accessible to site visitors.",
-    tecnologies: ["React", "JavaScript", "Node JS", "Firebase" , "Css"],
-    deploy: "https://baxinova.netlify.app/#",
-    repo: "https://github.com/luk444/Web-Empresa-Calderas",
-    img: baxinova,
-    rol: "FrontEnd"
+      "I developed a complete website to promote web development services to entrepreneurs who want to create their website and various development plans.",
+    tecnologies: ["Javascript", "React JS", "Tailwind"],
+    deploy: "https://developer-web-service.netlify.app",
+    repo: "https://github.com/luk444/webservicedev",
+    img: webservicedev,
+    rol:"Full Stack Developer"
+  },
+  {
+    title: "Development of Courses publish Platform",
+    date: "Feb 2025",
+    description:
+      "I developed a comprehensive website to publish courses/trainings and allow users to purchase them. I included an admin panel and dashboard to manage the courses. Login/Register.",
+    tecnologies: ["Javascript", "React JS", "Redux", "Tailwind", "Node JS", "Firebase", "FirebaseAuth"],
+    deploy: "https://webcourseonlinee.netlify.app/",
+    repo: "https://github.com/luk444/webcourseonlinee",
+    img: cursoweb,
+    rol:"Full Stack Developer"
+  },
+  {
+    title: "Development of Auto Parts Ecommerce Platform",
+    date: "Jun 2024",
+    description:
+      "I developed a complete e-commerce website for an automotive parts retailer. It includes a dashboard and admin panel to manage products, purchase status, and many other features.",
+    tecnologies: ["Javascript", "React JS", "Tailwind", "Node JS", "Firebase", "FirebaseAuth"],
+    deploy: "https://autopartesalfa.netlify.app",
+    repo: "https://github.com/luk444/Ecomerce-AutoManiacos",
+    img: autopartsweb,
+    rol:"Full Stack Developer"
+  },
+  {
+    title: "Development of Web Calderas Service",
+    date: "Jun 2024",
+    description:
+      "I developed a website for a boilermaking company specializing in heating. This was to promote its services and allow potential customers to more easily manage a query online and then be directed to a contact source to complete a technician's visit.",
+    tecnologies: [ "React JS", "tailwind", "Tailwind", "Node JS" ],
+    deploy: "https://svsrmxzzy4moteyu.vercel.app/",
+    repo: "https://github.com/luk444/Calderas-web",
+    img: calderasweb,
+    rol:"Full Stack Developer"
+  },
+  {
+    title: "Development.of Furniture Ecommerce",
+    date: "Sep 2022 - update Feb-2025",
+    description:
+      "I developed a complete e-commerce website for an upholstery and furniture business. It includes: login/register, a dashboard and admin panel to manage products, purchase/shipping status, and many other features.",
+    tecnologies: ["React", "JavaScript", "Node JS", "Firebase" , "FirebaseAuth", "tailwind"],
+    deploy: "https://hogar-muebles.netlify.app",
+    repo: "https://github.com/luk444/ReactJs-final",
+    img: mueblesweb,
+    rol: "Full Stack Developer"
   },
   {
     "title": "E-commerce 3D Simulator",
@@ -49,7 +96,7 @@ const proyects = [
     tecnologies: ["Javascript", "React", "Tailwind"],
     deploy: "https://beauty-salon-sandy.vercel.app/",
     repo: "https://github.com/luk444/Beauty-salon",
-    img: Classyclub,
+    img: classyclub,
   },
   {
     "title": "iDeleteCloud - Simulator",
@@ -68,20 +115,10 @@ const proyects = [
     tecnologies: ["Javascript", "React JS", "Tailwind", "API"],
     deploy: "https://trade-cripto.vercel.app/trade",
     repo: "hhttps://github.com/luk444/TradeCripto",
-    img: TradeCripto,
+    img: tradeCripto,
     rol:"Full Stack Developer"
   },
-  {
-    title: "Development.of Furniture Ecommerce",
-    date: "Sep 2022",
-    description:
-      "I developed a furniture ecommerce using ReactJS as a final project. I was responsible for both front-end and back-end, ensuring a smooth user experience and robust functionalities such as inventory management and order processing. This project represented my ability to apply ReactJS in creating interactive and efficient interfaces, demonstrating proficiency in complete web application development from planning to implementation.",
-    tecnologies: ["React", "JavaScript", "Node JS", "Firebase" , "Css"],
-    deploy: "https://bejewelled-begonia-49bed3.netlify.app/",
-    repo: "https://github.com/luk444/ReactJs-final",
-    img: furniture,
-    rol: "Full Stack Developer"
-  },
+
   {
     title: "Lucc Translator [not working because of expired api key]",
     date: "July 2022",
